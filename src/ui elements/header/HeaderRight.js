997784139ed8a90a4d595f7ac9ed74a867398c27
Max Girteit <icufishmg@gmail.com>
2022-05-17 22:01:37 -0600

    header right icons set

--- src/ui elements/header/HeaderRight.js
+++ src/ui elements/header/HeaderRight.js
@@ -1,9 +1,23 @@
-import React from 'react'
+import React from 'react';
+import { Box } from '@mui/system';
+import AppsIcon from '@mui/icons-material/Apps';
+import NotificationsIcon from '@mui/icons-material/Notifications';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+const styles = {
+  icons: {
+    pr: '60px',
+  },
+};
 
 const HeaderRight = () => {
   return (
-    <div>HeaderRight</div>
-  )
-}
+    <Box sx={styles.icons}>
+      <AppsIcon />
+      <NotificationsIcon />
+      <AccountCircleIcon />
+    </Box>
+  );
+};
 
-export default HeaderRight
+export default HeaderRight;
