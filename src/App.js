41e37969258e37a819e86e4e565998776c930e16
Max Girteit <icufishmg@gmail.com>
2022-05-29 15:17:50 -0600

    sidebar added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
 import React from 'react';
 
-
 import Header from './ui elements/header/Header';
+import Sidebar from './ui elements/sidebar/Sidebar';
 
 import './App.css';
+
 
 function App() {
   return (
     <div className="app">
       <Header />
+      <Sidebar  />
     </div>
   );
 }
